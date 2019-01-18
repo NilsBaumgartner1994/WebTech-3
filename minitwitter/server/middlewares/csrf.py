1d5bfadb9ad789f79ec88aea80b710601276394c
Unknown <fizzule@hotmail.de>
2019-01-18 13:43:36 +0100

    CSRF Check

--- minitwitter/server/middlewares/csrf.py
+++ minitwitter/server/middlewares/csrf.py
@@ -14,13 +14,33 @@ class CSRFMiddleware(webserver.Middleware):
 
     def process_request(self, request, response):
         """"""
-        pass
+        if self.token == "No CSRF Token!":
+            return self.wrong_csrf()
+        if request.method == 'POST' or request.method == 'post':
+            if cookiename not in request.cookies:
+                return self.wrong_csrf()
+            if request.cookies[cookiename] == "No CSRF Token!":
+                return self.wrong_csrf()
+            if not request.cookies[cookiename] == self.token:
+                return self.wrong_csrf()
+        else:
+            if cookiename not in request.cookies:
+                self.token = self.create_token()
+                response.add_cookie(self.make_cookie(self.token))
 
     def process_response(self, response):
         """Add CSRF Token"""
         response.add_cookie(self.make_cookie(self.token))
 
+    def create_token(self):
+        return "Created Token"
+
     def make_cookie(self, value):
         """Returns Cookie object for nightmode"""
         return webserver.Cookie("csrftoken", value, path='/')
         #return webserver.Cookie(self.cookiename, value, path='/', expires=webserver.Cookie.expiry_date(30))
+
+    def wrong_csrf(self):
+        """ Respond that no Authentification is given/there"""
+        log(3, "Wrong CSRF-Token.")
+        raise webserver.StopProcessing(403, "Wrong CSRF-Token.")
